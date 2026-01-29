0d761532ce4c632b8a78b240f9194d1e3b3d236c
Shynitha Muthyam <shynithamuthyam@Mac.lan>
2026-01-28 22:31:46 -0500

    link changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio.github.io/',
+  base: "/portfolio/",
   assetsInclude: ['**/*.PNG', '**/*.png', '**/*.JPG', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
 })
